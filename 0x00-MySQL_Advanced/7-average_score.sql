ad739460a30613891d1f24fef1675148414f3ebd
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-19 04:51:21 +0300

    index

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,18 +1,13 @@
 -- Create the stored procedure ComputeAverageScoreForUser
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN p_user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = p_user_id;
-
-    INSERT INTO users (id, average_score) VALUES (p_user_id, avg_score)
-    ON DUPLICATE KEY UPDATE average_score = avg_score;
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 END;
 
 //
