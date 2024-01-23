4a7e0a5ae8c6f29a86ebc21f222db9303090f0fd
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-23 22:37:37 +0300

    log stats

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""11-schools_by_topic.py"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    retrieves the list of schools having a specific topic
+    """
+    return mongo_collection.find({ "topics": topic })
