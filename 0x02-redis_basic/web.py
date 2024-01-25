d3256708c9a02b1452543e818d610832bc7b1c02
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-25 05:14:03 +0300

    expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,42 +1,36 @@
+#!/usr/bin/env python3
+"""
+web cache and tracker
+"""
 import requests
 import redis
-import functools
+from functools import wraps
+
+store = redis.Redis()
 
 
-redis_client = redis.Redis()
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
+        count_key = "count:" + url
+        html = method(url)
 
-def track_access(method):
-    """
-    Decorator to track the number of times a particular URL is accessed.
-    """
-    @functools.wraps(method)
-    def wrapper(url, *args, **kwargs):
-        access_count_key = f"count:{url}"
-        redis_client.incr(access_count_key)
-
-        result = method(url, *args, **kwargs)
-
-        return result
-
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@track_access
+@count_url_access
 def get_page(url: str) -> str:
-    """
-    Obtain the HTML content of a particular URL and cache the result
-    with an expiration time of 10 seconds.
-    """
-    cache_key = f"cache:{url}"
-    cached_result = redis_client.get(cache_key)
-
-    if cached_result is not None:
-        return cached_result.decode('utf-8')
-
-    response = requests.get(url)
-    html_content = response.text
-
-    redis_client.setex(cache_key, 10, html_content)
-
-    return html_content
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
